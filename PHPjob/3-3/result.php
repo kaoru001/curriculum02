24af7beea73b56bdf35a981b27706fd0c9476e0d
kaoru001 <64976586+kaoru001@users.noreply.github.com>
2022-06-30 01:15:42 +0900

    Update result.php

--- PHPjob/3-3/result.php
+++ PHPjob/3-3/result.php
@@ -4,24 +4,27 @@
     $my_number = $_POST['my_number'];
     date_default_timezone_set('Asia/Tokyo');
     // echo date("Y-m-d H:i:s")
+    $hairetu = str_split($my_number);
+    $key = array_rand($hairetu);
+    $number = $hairetu[$key];
     ?>
     
     <p><?php echo date("Y/m/d"); ?>の運勢は</p>
 
     <?php
-    if($my_number === 0){
-       echo '選ばれた数字は', $my_number, '<br>';
+    if($number === 0){
+       echo '選ばれた数字は', $number, '<br>';
        echo '凶';
-    }else if($my_number <= 3){
-        echo '選ばれた数字は', $my_number, '<br>';
+    }else if($number <= 3){
+        echo '選ばれた数字は', $number, '<br>';
         echo '小吉';
-    }else if($my_number <= 6){
-        echo '選ばれた数字は', $my_number, '<br>';
+    }else if($number <= 6){
+        echo '選ばれた数字は', $number, '<br>';
         echo '中吉';
-    }else if($my_number <= 8){
-        echo '選ばれた数字は', $my_number, '<br>';
+    }else if($number <= 8){
+        echo '選ばれた数字は', $number, '<br>';
         echo '吉';
-    }else if($my_number <= 9){
-        echo '選ばれた数字は', $my_number, '<br>';
+    }else if($number <= 9){
+        echo '選ばれた数字は', $number, '<br>';
         echo '大吉';
     }
